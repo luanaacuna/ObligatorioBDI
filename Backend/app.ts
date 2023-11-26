5325a7e44d43f9d9f57f3a6faf8f20632ff603ad
JavierOdera <javier.odera@correo.ucu.edu.uy>
2023-11-26 15:24:40 -0300

    Al fin funciona el backend

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,37 +1,40 @@
-// @ts-nocheck
-import express from 'express';
-
+import express from "express";
+import router from "./api/info";
 import bodyParser from "body-parser";
-const app  = express()
-app.listen('3307', function(){
-    console.log("el nuevo funciona")
-})
 
+const app = express();
+var corsOptions = {
+  origin: ["http://localhost:3306", "http://127.0.0.1:3306"],
+};
+const cors = require("cors");
+app.use(cors(corsOptions));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(router);
 
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
 
+app.listen("3307", function () {
+  console.log("backend funciona");
+});
 
-const mysql = require('mysql')
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Contraseña123',
-  database: 'my_db',
-  port: 3306
-})
+const mysql = require("mysql");
+export const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "Contraseña123",
+  database: "my_db",
+  port: 3306,
+});
 
-connection.connect()
-connection.use(infoApi)
+connection.connect();
 
+connection.query("SELECT 1 + 1 AS solution", (err: Error) => {
+  if (err) throw err;
 
-//app.use(infoApi);
+  console.log("Base de datos conectada en puerto ", connection.config.port);
+});
 
-
-
-
-connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
-  if (err) throw err
-
-  console.log('Base de datos conectada en puerto ', connection.config.port )
-})
-
-connection.end()
+//connection.end();
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # ObligatorioBDI
+
+El script se encuentra en Script_creacion_tablas.txt
+
+1- Abrir el repositorio e instalar los paquetes necesarios con NPM install.
+2- Levantar una base de datos y modelarla con el script proporcionado
+3- Levantar el back endentrando a dicha carpeta y corriendo el comando 'npm run start'
+4- Levantar el front end entrando a su carpeta y corriendo el comando 'ng serve --open'
+5- registrar un usuario un usuario o logearse con uno existente
+6- Rellenar los datos de ser necesario
+
+
+
+
+
